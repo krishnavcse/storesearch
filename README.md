2e7b93ed55534fbecf3c6e10a728e3172d172f62
krishnavcse <36372467+krishnavcse@users.noreply.github.com>
2018-02-11 14:58:43 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# storesearch
-A Simple python Script to store Google search results URL in a separate file named search.txt . The search results are stored in the database.
+Store your Search
+========================
+
+A Simple python Script to store Google search results URL in a separate file named search.txt .
+The search results are stored in the database.
+Once the search results are stored in the database, it is printed on the command line.
+Used MongoDB for NOSQL database.
+BeautifulSoup4 library has been used to get the data from HTML.
+The Seagle code is executed using command line.
+
+## Install requirements
+
+$sudo ./INSTALL
+
+## Run script
+
+$python Seagle.py keyword*
+
+*keyword is the word to be searched through Google Search engine.
+
+NOTE: This script was developed for specific requirement. Please Modify it as per your requirement and use to search from different search engine.
+
+
+
+
---
+++ Seagle.py
@@ -0,0 +1,35 @@
+import requests, sys, bs4
+from pymongo import MongoClient
+client = MongoClient('localhost:27017')
+db = client.WebData
+
+x=input("Enter the no. of Search results u require(0<x<=10):")
+print('Hunting.......')
+# Download the search page
+res = requests.get("https://google.co.in/search?q=" + ''.join(sys.argv[1:]))
+res.raise_for_status()
+# Pull data out from the html
+soup = bs4.BeautifulSoup(res.text)
+# Select all the search links
+linkElems = soup.select('.r a')
+f=open("search.txt","w+")
+# Store and print the top x google search results
+for i in range(x):
+    link = linkElems[i].get('href')
+    print ('\n')
+    f.write(link)
+    f.write("\r\n")
+    print (link)
+    print('\n')
+    db.Websites.insert_one(
+        {
+            'link':link
+        }
+    )
+#f.close()
+print('Data is Inserted to file successfully.\n')
+# Dispalys the stored data from the database on command line
+webcom = db.Websites.find()
+print("complete data from database:\n")
+for web in webcom:
+    print(web)
